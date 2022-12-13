0349ef08e4f49deb5dcf67a8d97677757616c780
huseyinyasarr <huseyin_yasar20@erdogan.edu.tr>
2022-12-13 14:49:44 +0300

    added

---
+++ ce205-hw4-test-cs/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ce205-hw4-test-cs")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ce205-hw4-test-cs")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ed9a8c02-db31-47fa-abeb-38babba400b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ce205-hw4-test-cs/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using static ce205_hw4_algorithms_cs.Class1;
+
+
+namespace ce205_hw4_test_cs
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Open_Addressing_LinearProbing_Test1()
+        {
+            HashOps hashOps = new HashOps();
+            hashOps.OpenTable();
+            string LoremText = "Sed eu tristique est. Class aptent taciti sociosqu ad litora.";
+            string[] LoremArray = LoremText.Split();
+            for (int i = 0; i < LoremArray.Length; i++)
+                hashOps.LinearInsert(i + 1, LoremArray[i]);
+            string result = hashOps.GetDataLinearProbing(1);
+            string expected = "Sed";
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Open_Addressing_QuadraticProbing_Test1()
+        {
+            HashOps hashOps = new HashOps();
+            hashOps.OpenTable();
+            string LoremText = "Vestibulum et urna velit. Aenean vitae porttitor mi. Phasellus at.";
+            string[] LoremArray = LoremText.Split();
+            for (int i = 0; i < LoremArray.Length; i++)
+                hashOps.QuadraticInsert(i + 1, LoremArray[i]);
+            string result = hashOps.GetDataQuadraticProbing(1);
+            string expected = "Vestibulum";
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void Open_Addressing_DoubleHashing_Test1()
+        {
+            HashOps hashOps = new HashOps();
+            hashOps.OpenTable();
+            string LoremText = "Donec augue arcu, venenatis interdum blandit at, pretium sed arcu.";
+            string[] LoremArray = LoremText.Split();
+            for (int i = 0; i < LoremArray.Length; i++)
+                hashOps.DoubleHashing(i + 1, LoremArray[i]);
+            string result = hashOps.GetDataDoubleHashing(8);
+            string expected = "pretium";
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
